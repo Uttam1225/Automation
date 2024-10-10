1657592f1b46dcd9df3692a317428ffe07b4e85d
Uttam1225 <45071838+Uttam1225@users.noreply.github.com>
2024-10-10 20:28:30 +0530

    Feature/code using chatgpt for gfg potd (#5)
    * chatgpt code but not completed yet
    
    * LinkedIn code
    
    ---------
    
    Co-authored-by: monika0311 <monikamehta0311@gmail.com>

--- src/main/java/constants/AppConstants.java
+++ src/main/java/constants/AppConstants.java
@@ -8,4 +8,8 @@ public class AppConstants {
     public static final String GFG_TITLE = "Problem Of The Day";
     public static final String INITIAL_CHATGPT_TEXT = "Provide only code in java for this problem";
     public static final String FINAL_CHATGPT_TEXT = "on GeeksForGeeks Problem Of The Day";
+    public static final String DESIGNATION = "SDET Engineer";
+    public static final String EXPERIENCE = "3 years";
+    public static final String SEARCH_PEOPLE = "Talent acquisition";
+    public static final String NOTE_FOR_PEOPLE_LINKED_IN = "Hi, I’m impressed by your work in talent acquisition and would love to connect to explore opportunities and share insights.";
 }
--- src/test/java/tests/JobApply.java
+++ src/test/java/tests/JobApply.java
@@ -1,21 +1,43 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import pages.JobApplyPage;
 import org.testng.annotations.Test;
+import pages.LoginPage;
+import utils.ConfigReader;
+import utils.FetchAWSCreds;
+import utils.SensitiveDataHandler;
+
+import static constants.AppConstants.DRIVER_TITLE;
+import static constants.AppConstants.USERNAME;
 
 public class JobApply extends BaseTest {
+    ConfigReader config = new ConfigReader();
 
+    @BeforeMethod
+    public void intialize() {
+        setUp(config.returnNaukriUrl());
+    }
 
-
+    public void testValidLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        //creating object for Sensitive Data Handler class
+        loginPage.clickLoginButton();
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+        loginPage.clickSignInButton();
+        // Assert condition for successful login to correct creds
+        loginPage.isLoginSuccessful();
+    }
     @Test
-    public void searchAndApplyJobsOnNaukri()
-    {
+    public void searchAndApplyJobsOnNaukri() throws InterruptedException {
         JobApplyPage jobApply = new JobApplyPage(driver);
-        LoginTest login = new LoginTest();
-        login.testValidLogin();
+        testValidLogin();
         jobApply.clickOnSearchButton();
-        jobApply.enterSearchingDesignationAndExperience("SDET engineer", "3 years");
-        jobApply.applyJobsOnNaukri();
+        jobApply.enterSearchingDesignationAndExperience();
+        Thread.sleep(10000);
+        //jobApply.applyJobsOnNaukri();
     }
 }
